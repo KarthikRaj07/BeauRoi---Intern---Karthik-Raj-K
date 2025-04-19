53334dcddf77a4d200b9571894778eefe0598376
Your Name <you@example.com>
2025-04-19 21:11:04 +0530

    spring backend initial commit

---
+++ SKS-backend/src/main/java/com/skillRec/SKS_backend/SksBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skillRec.SKS_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SksBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SksBackendApplication.class, args);
+	}
+
+}
---
+++ SKS-backend/src/test/java/com/skillRec/SKS_backend/SksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillRec.SKS_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
